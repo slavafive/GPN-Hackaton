021d5c2b2c5f5dd435d8eb0890d7c812ea0cb75f
Bey Sviatoslav Igorevich <bey.sviatoslav@yandex.ru>
2021-08-28 23:47:44 +0300

    Fix bugs.

--- app_runner.py
+++ app_runner.py
@@ -1,13 +1,19 @@
-import flask
+from flask import Flask, request, render_template
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return flask.render_template('index.html', companies=get_companies(), site_title="Camposha.info")
+    key_words = request.args.get('text')
+    if key_words == None:
+        #Код обучения
+    else:
+        #query()
+    print(key_words)
+    return render_template('index.html', companies=get_companies(key_words))
 
-def get_companies():
-    companies = [{'Title':'ООО Реформа', 'Address':'Улица', 'Phone': '+79217777777', 'Email':'email@gmail.com', 'Website':'www.site.com'}, {'Title':'ООО Реформа', 'Address':'Улица', 'Phone': '+79217777777', 'Email':'email@gmail.com', 'Website':'www.site.com'}, {'Title':'ООО Реформа', 'Address':'Улица', 'Phone': '+79217777777', 'Email':'email@gmail.com', 'Website':'www.site.com'}]
+def get_companies(key_words):
+    companies = [{'Title':f'ООО {key_words}', 'Address':'Улица', 'Phone': '+79217777777', 'Email':'email@gmail.com', 'Website':'www.site.com'}, {'Title':'ООО Реформа', 'Address':'Улица', 'Phone': '+79217777777', 'Email':'email@gmail.com', 'Website':'www.site.com'}, {'Title':'ООО Реформа', 'Address':'Улица', 'Phone': '+79217777777', 'Email':'email@gmail.com', 'Website':'www.site.com'}]
     return companies
 
 if __name__ == '__main__':
