c046610b1fb4821ede87e62b54f568a24fa12e65
slavafive <vyacheslavefimov1999@gmail.com>
2021-08-28 19:14:05 +0300

    Internet parsing implementation

---
+++ ml/vectorizer.py
@@ -0,0 +1,18 @@
+import numpy as np
+import ru_core_news_md
+
+nlp = ru_core_news_md.load()
+
+
+def vectorizer_sentence(text):
+    return nlp(text).vector
+
+
+def vectorize_corpus(corpus):
+    with nlp.disable_pipes():
+        vectors = np.array([nlp(text).vector for text in corpus])
+    return vectors
+
+
+if __name__ == 'main':
+    pass
